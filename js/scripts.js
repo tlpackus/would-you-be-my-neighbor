51c73b5b4b9a13c940172db79a8ffa2494d5964f
tlpackus <tlpackus@gmail.com>
2021-02-18 18:35:58 -0800

    add spacing between index values

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ function beepBoop(inputNumber) {
     let resultArray = [];
   for (let index = 0; index <= inputNumber; index ++) {
     if ([index].toString().includes(3)) {
-      resultArray.push("Won't you be my neighbor?");
+      resultArray.push(" Won't you be my neighbor?");
     } else if ([index].toString().includes(2)) {
-      resultArray.push("Boop!");
+      resultArray.push(" Boop!");
     } else if ([index].toString().includes(1)) {
-      resultArray.push("Beep!");
+      resultArray.push(" Beep!");
     } else {
       resultArray.push(index);
     };
@@ -20,6 +20,6 @@ $(document).ready(function() {
     let inputNumber = $("input#number").val();
     let result = beepBoop(inputNumber);
 
-    $("#result").text(result);
+    $("#result").text(" " + result);
   });
 });
