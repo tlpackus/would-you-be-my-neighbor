fd5ce1f9f1fc59065f3457e188ccffb7ca1172e5
tlpackus <tlpackus@gmail.com>
2021-02-18 18:32:36 -0800

    this adds a function called beepBoop to check if each number before the inputed number contains a 3, 2, or 1 and the replaces them with text if they do. I had it working a while ago but it wouldnt submit the results to where they were visible on the page. After a couple hours of trial and error i found out i was calling the id of result apposed to the class so i changes the class to id instead and now it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
-// function splitToArray(number) {
-//   const array = [];
-
-// 
-
-
-
-
+function beepBoop(inputNumber) {
+    let resultArray = [];
+  for (let index = 0; index <= inputNumber; index ++) {
+    if ([index].toString().includes(3)) {
+      resultArray.push("Won't you be my neighbor?");
+    } else if ([index].toString().includes(2)) {
+      resultArray.push("Boop!");
+    } else if ([index].toString().includes(1)) {
+      resultArray.push("Beep!");
+    } else {
+      resultArray.push(index);
+    };
+  };
+    return resultArray;
+};
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
-    const inputNumber = parseInt($("input#number").val());
+    let inputNumber = $("input#number").val();
+    let result = beepBoop(inputNumber);
 
-    $("#result").text
-  })
-})
+    $("#result").text(result);
+  });
+});
